78925b3b665b96de1d05fefa1525cf7ac6805138
rootslab <44gatti@gmail.com>
2014-05-06 20:09:32 +0200

    Updated Readme. On branch master modified:   example/example.js

--- example/example.js
+++ example/example.js
@@ -1,9 +1,13 @@
 var log = console.log
     , assert = require( 'assert' )
     , util = require( 'util' )
+    // get Syllabus in develop mode
     , Syllabus = require( '../' )( true )
+    // commands
     , commands = Syllabus.commands
+    // types
     , types = Syllabus.types
     ;
 
+// log all Syllabus properties
 log( util.inspect( Syllabus, false, 3, true ) );
