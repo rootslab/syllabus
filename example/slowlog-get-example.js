6b7f7a2ee054300b27a23a644ddb95b2b48b7ffc
rootslab <44gatti@gmail.com>
2014-05-09 10:39:11 +0200

    v 0.11.0. Updated Bolgia dependency to v2.5.0. Changed default utility fn to Bolgia#reveal. Added slowlog-example.js. On branch master new file:   example/slowlog-get-example.js modified:   lib/commands/server.js modified:   package.json

---
+++ example/slowlog-get-example.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+/* 
+ * Syllabus parse SLOWLOG GET reply example
+ */
+
+var log = console.log
+    , util = require( 'util' )
+    // get Syllabus in develop mode
+    , Syllabus = require( '../' )()
+    , commands = Syllabus.commands
+    , isArray = Array.isArray
+    // convert all items to Buffers
+    , convert = function ( arr ) {
+        var a = arr || []
+            , alen = a.length
+            , el = a[ 0 ]
+            , i = 0
+            ;
+        for ( ; i < alen; el = a[ ++i ] ) {
+            if ( isArray( el ) ) {
+                convert( el );
+                continue;
+            }
+            a[ i ] = new Buffer( String( el ) );
+        };
+        return a;
+    }
+    // use a SLOWLOG GET reply from Redis
+    , slowlog_reply = [ [ '14', '1309448221', '15', [ 'PING' ] ], [ '13', '1309448128', '30', [ 'SLOWLOG', 'GET', '100' ] ] ]
+    , slowlog_data = convert( slowlog_reply )
+    , expected_reply = [ [ 14, 1309448221, 15, [ 'PING' ] ], [ 13, 1309448128, 30, [ 'SLOWLOG', 'GET', 100 ] ] ]
+    // build command obj/hash
+    , scmd = commands.slowlog.get( 0 )
+    // reveal Buffer items with default utility fn ( Bolgia#reveal )
+    , list = scmd.fn( slowlog_data )
+    ;
+
+log( '\n- slowlog expected reply:\n' );
+log( util.inspect( expected_reply, false, 2, true ), '\n' );
+
+log( '\n- slowlog reply:\n' );
+log( util.inspect( list, false, 2, true ), '\n' );
